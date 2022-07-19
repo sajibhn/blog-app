a25a4096a4864b6ed1538807a62d85c250550585
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-19 15:34:04 +0600

    added delete blog functionality

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -68,3 +68,18 @@ export const getById = async (req, res, next) => {
     }
     return res.status(200).json({ blog })
 }
+
+export const deleteBlog = async (req, res, next) => {
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndRemove(id)
+    } catch (err) {
+        return console.log(err)
+    }
+
+    if (!blog) {
+        return res.status(500).json({ message: "unable to delete" })
+    }
+    return res.status(200).json({ message: "Successfully deleted" })
+}
