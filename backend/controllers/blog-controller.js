bb38871004d49006683aad7dd957217df7e2a568
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-19 15:29:22 +0600

    added get blog by id func

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -51,3 +51,20 @@ export const updateBlog = async (req, res, next) => {
 
     return res.status(200).json({ blog })
 }
+
+export const getById = async (req, res, next) => {
+    const id = req.params.id;
+
+    let blog;
+
+    try {
+        blog = await Blog.findById(id);
+    } catch (err) {
+        return console.log(err)
+    }
+
+    if (!blog) {
+        return res.status(404).json({ message: "No Blog Found" })
+    }
+    return res.status(200).json({ blog })
+}
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addBlog, getAllBlogs, updateBlog } from '../controllers/blog-controller.js'
+import { addBlog, getAllBlogs, getById, updateBlog } from '../controllers/blog-controller.js'
 const blogRouter = express.Router()
 
 blogRouter.get("/", getAllBlogs)
 blogRouter.post("/add", addBlog)
 blogRouter.put("/update/:id", updateBlog)
+blogRouter.get("/:id", getById)
 
 export default blogRouter
